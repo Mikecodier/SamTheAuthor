6eda24d47a88bb5bc6f30472d9a6af1f84af9ced
Mikecodier <123463717+Mikecodier@users.noreply.github.com>
2023-07-08 19:00:49 +0100

    Add files via upload

---
+++ Landing-page/script.js
@@ -0,0 +1,113 @@
+console.log("Hello world");
+
+// Set current year
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+// Mobile Navigation
+
+const btnNavEl = document.querySelector(".mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+/////////////////////////////////////////////// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    if (href !== "#" && href.startsWith("#")) console.log(href);
+
+    // Scroll to other link
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+
+    if (href === "https://www.amazon.com/") {
+      window.location.href = "https://www.amazon.com/";
+    }
+
+    if (href === "https://www.amazon.com/") {
+      window.open("https://www.amazon.com/", "_blank");
+    }
+
+    if (href === "https://www.shopify.com/") {
+      window.location.href = "https://www.shopify.com/";
+    }
+
+    if (href === "https://www.shopify.com/") {
+      window.open("https://www.shopify.com/", "_blank");
+    }
+
+    if (href === " https://www.smashwords.com/") {
+      window.location.href = " https://www.smashwords.com/";
+    }
+
+    if (href === " https://www.smashwords.com/") {
+      window.open(" https://www.smashwords.com/", "_blank");
+    }
+  });
+});
+
+///////////////////////////////////////////////Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
